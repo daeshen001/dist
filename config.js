1b74401edfd5381ad65b8d843428fbf76e6dd6d2
songe <1497354161@qq.com>
2019-02-20 17:08:13 +0800

    Initial commit

---
+++ config.js
@@ -0,0 +1,7 @@
+window.$config = {
+    SERVER_ADDRESS: 'http://127.0.0.1:6060',
+    IMG_ADDRESS: 'http://picture-files.oss-cn-qingdao.aliyuncs.com/',
+    PERMISSION_BUTTON: true,
+    PERMISSION_API: true,
+    PLATFORM: 'JIYU'
+};
